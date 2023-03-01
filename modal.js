0d890136842afcd8397a65392eb6bd1c5e03c05d
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-03-01 12:06:08 +0100

    clean files

--- modal.js
+++ modal.js
@@ -1,23 +1,23 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var header = document.getElementById("header");
+  if (header.className === "header") {
+    header.className += " responsive";
   } else {
-    x.className = "topnav";
+    header.className = "header";
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const formSection = document.querySelector(".form-section");
+const signupBtn = document.querySelectorAll(".btn-signup");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+signupBtn.forEach((btn) => btn.addEventListener("click", launchForm));
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function launchForm() {
+  formSection.style.display = "block";
 }
 
 
