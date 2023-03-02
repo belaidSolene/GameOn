8f9890b17869f4981b5e0c7d483b7aec978a9bb3
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-03-02 14:22:12 +0100

    add error msg + closing modal

--- modal.js
+++ modal.js
@@ -7,17 +7,58 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Modal Elements for Modal Events
 const modalSection = document.querySelector(".modal-section");
-const signupBtn = document.querySelectorAll(".btn-signup");
-const formData = document.querySelectorAll(".formData");
+const signupBtn = document.querySelectorAll(".btn--signup");
+const closeModalBtn = document.querySelector(".close-form");
+const formBody = document.querySelector(".modal-section__form-body");
+const firstName = document.getElementById("firstName");
 
+// Modal Events
 // launch modal event
-signupBtn.forEach((btn) => btn.addEventListener("click", launchForm));
+signupBtn.forEach((btn) => btn.addEventListener("click", lauchModal));
 
-// launch modal form
-function launchForm() {
-  modalSection.style.display = "block";
+// close modal with button 
+closeModalBtn.addEventListener("click", closeModal(modalSection));
+
+// launch modal modal
+function lauchModal() {
+  modalSection.className += " show";
+  firstName.focus(); // focus on firstName
 }
 
+// close modal
+function closeModal(element) {
+ element.classList.remove("show");
+}
+
+
+// Form Elements for Validation Form
+const formData = document.querySelectorAll(".formData");
+
+const form = document.getElementById("bookingGameEvent");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthDate");
+const numberGameJoin = document.getElementById("numberGameJoin");
+const checkboxCGU = document.getElementById("checkboxCGU");
+
+// Validation Form
+form.addEventListener('submit', (event) => {
+  event.preventDefault;
+  // close modal ?? if yes use the function else delete the function and put the line direct in the event
+});
+
+const dataError = document.getElementById("magic");
+
+dataError.addEventListener('click', () => {
+  for (const div of formData) {
+    console.log(div); // affiche toutes les div formData
+  }
+  // Possible d'ajouter data-error-visible 
+  // comment déterminer que c'est sur la bonne div qu'il la modifie ??
+  // il faut
+  // found la div dont l'enfant "input" à "id === falseElement.id"
+  //et ajouter l'attribut data-eror-visible à la div
+});
 
