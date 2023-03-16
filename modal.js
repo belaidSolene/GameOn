77b6bd9b94fb4dae3c353ba9ec9ac69a840d030c
Belaid Solene <solene.belaid@greta-cfa-aquitaine.academy>
2023-03-16 14:55:57 +0100

    validationFields OK

--- modal.js
+++ modal.js
@@ -7,28 +7,33 @@ function editNav() {
   }
 }
 
+modalEvents();
 
-// Modal Elements for Modal Events
-const modalSection = document.querySelector(".modal-section");
-const signupBtn = document.querySelectorAll(".btn--signup");
-const closeModalBtn = document.querySelector(".close-form");
 
-// Modal Events
-// launch modal event
-signupBtn.forEach((btn) => btn.addEventListener("click", lauchModal));
 
-// close modal with button 
-closeModalBtn.addEventListener("click", closeModal);
+function modalEvents() {
+  // Modal Elements
+  const modalSection = document.querySelector(".modal-section");
+  const signupBtn = document.querySelectorAll(".btn--signup");
+  const closeModalBtn = document.querySelector(".close-form");
 
-// launch modal modal
-function lauchModal() {
-  modalSection.className += " show";
+  // launch modal event
+  signupBtn.forEach((btn) => btn.addEventListener("click", lauchModal));
+
+  // close modal with button 
+  closeModalBtn.addEventListener("click", closeModal);
+
+  // launch modal
+  function lauchModal() {
+    modalSection.className += " show";
+  }
+
+  // close modal
+  function closeModal() {
+    modalSection.classList.remove("show");
+  }
 }
 
-// close modal
-function closeModal() {
-  modalSection.classList.remove("show");
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Form Element for Validation Form
@@ -44,162 +49,81 @@ form.addEventListener('submit', (event) => {
 
 //Experience to make form validation
 const dataError = document.getElementById("magic"); // bouton/texte pour lancer les fonctions
+
 dataError.addEventListener('click', () => {
-  const allFormData = document.querySelectorAll(".formData");
-
-  const tabFieldsForm = getFieldsForm(allFormData);
-
-  for (const fieldForm of tabFieldsForm) {
-    validateInput(fieldForm, allFormData);
-  }
+  validationFields();
 });
 
-  // objectif : Récupérer dans un tableau chaque input du formulaire bookingGameEvent \\
-function getFieldsForm(allFormData) {
-    
-  let tabInput = [];
-  let tabRadio = [];
+function validationFields() {
+  const validateProcess = new Map([
+    ['firstName', [isEmpty, isPatternRespected]], //pas vide, 2 caract
+    ['lastName', [isEmpty, isPatternRespected]],
+    ['email', [isEmpty, isPatternRespected]], //valide
+    ['numberGameJoin', [isPatternRespected]], //not empty, chiffre
+    ['location', [isCheckedRadio]], // un bouton sélectionné
+    ['checkboxCGU', [isCheckedCheckbox]] //obligatoire
+  ]);
 
-  for (const div of allFormData) {
-    const childrenFormData = div.childNodes;
+  validateProcess.forEach((fcts, key) => {
+    const fields = document.getElementsByName(key);
+    const msgErreur = [];
 
-    if (childrenFormData != null) {
-      for (const child of childrenFormData) {
+    fcts.forEach((fct) => {
+      const reponse = fct(fields);
 
-        if (child.nodeName === 'INPUT') { //faire deux tableaux ? un pour les inputs classiques et l'autre pour radio ?
-          //console.log('une réussite : ', child);
-          (child.type === 'radio' ? tabRadio : tabInput).push(child);
-        }
+      if(reponse != false) {
+        msgErreur.push(reponse);
       }
+    });
+
+    msgErreur.length >= 1 ? setDataError(getField(fields).parentNode, msgErreur[0]) :  deleteDataError(getField(fields).parentNode);
+  });
+}
+
+function isCheckedRadio(btnsRadios) {
+  let notChecked = true;
+
+  btnsRadios.forEach((radio) => {
+    if (radio.checked == true) {
+      notChecked = false;
     }
-  }
-   console.log('TabInput : ', tabInput);
-   console.log('Tab Radio : ', tabRadio);
-  return tabInput;
+  });
+
+  return notChecked ? "Veuillez choisir un tournoi" : false;
 }
 
-
-  // objectif : Déterminer quel input passé en argument pour effectuer par la suite la validation nécessaire
-function validateInput(field) {
-  let divParent = field.parentNode;
-  let nameInput = field.name;
-  let validity = field.validity;
-
-
-  switch (nameInput) {
-    case 'firstName':
-      console.log(nameInput, ' CheckValidity : ', field.checkValidity());
-      if (!field.checkValidity()) {
-        let msgError;
-        let empty = isEmpty(validity);
-
-        if (empty[0]) {
-          console.log(nameInput, ' avec isEmpty (on veut true) : ', empty[0]);
-          msgError = empty[1];
-        } else {
-          let short = isShort(validity);
-
-            if (short[0]) {
-              console.log(nameInput, ' avec isShort (on veut true) : ', short[0]);
-              msgError = short[1];
-            }
-        }
-        setDataError(divParent, msgError);
-      } else {
-        deleteDataError(divParent);
-      }
-
-      break;
-
-    case 'email':
-      if (!field.checkValidity()) {
-        let msgError;
-        let empty = isEmpty(validity);
-
-        if (empty[0]) {
-          console.log(nameInput, ' avec isEmpty (on veut true) : ', empty[0]);
-          msgError = empty[1];
-        } else {
-          let email = isEmail(validity);
-
-          if (email[0]) {
-            console.log(nameInput, ' avec isEmail : ', email[0]);
-            msgError = email[1];
-          }
-        }
-        setDataError(divParent, msgError);
-      } 
-      console.log(nameInput, ' avec typeMismatch : ', field.validity.typeMismatch);
-      break;
-
-  }
+function isCheckedCheckbox(fieldList) {
+  return !getField(fieldList).checked ? "veuillez accepter les CGU" : false;
 }
 
-/*
-const validateProcess = new Map ([
-  ['name', [FirstFunction, SecondFunction]],
-  ['email', [FirstFunction] ],
-  ['birthDate', [FistFunction, ThirdFunction] ]
-]);
-
-
-const FunctionNames = Object.freeze({ 
-  FirstFunction: "firstFunction", 
-  SecondFunction: "secondFunction" 
-});
-
-...
-
-var customObject = {
-  [FunctionNames.FirstFunction]: function(param){...},
-  [FunctionNames.SecondFunction]: function(param){...}
-};
-
-...
-
-customObject[FunctionNames.FirstFunction](param);
-*/
-
-
-function isValid(field) {
-  if (!field.checkValidity()) {
-    let errorFind = false;
-    let tabValidity = field.validity;
-    while (!errorFind) {
-      // isEmpty est appelé par défaut puis 
-      // parcours tab pour savoir quelles fonctions appelées 
-      // errorFind récupère la valeur à l'indice 0 du tab retourné
-    }
-  }
+function isEmpty(fieldList) {
+  return getField(fieldList).validity.valueMissing ? "ce champ est obligatoire" : false;
 }
 
-function isEmpty(validity) {
-  return validity.valueMissing ? [true, "ce champ est obligatoire"] : [false];
+function isPatternRespected(fieldList) {
+  const regex = new Map([
+    ['firstName', [/^[A-Za-zÀ-ÖØ-öø-ÿ\-\'\ ]{2,}$/, "il faut renseigner 2 caractères minimum"]],
+    ['lastName', [/^[A-Za-zÀ-ÖØ-öø-ÿ\-\'\ ]{2,}$/, "il faut renseigner 2 caractères minimum"]],
+    ['email', [/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/i, "email non valide"]],
+    ['numberGameJoin', [/^([0-9]{1,})$/, "obligatoire"]]
+  ]);
+
+  const field = getField(fieldList);
+  const value = regex.get(field.id);
+
+  return value[0].test(field.value) ? false : value[1];
 }
 
-function isShort(validity) {
-  return validity.tooShort ? [true, "il faut renseigner 2 caractères minimum"] : [false];
+function getField(fieldList) {
+  return field = fieldList[0];
 }
 
-function isEmail(validity) {
-  return validity.typeMismatch ? [true, "ce n'est pas un email"] : [false];
-}
-
-function isTypeRespected(validity) {
-  return validity.badInput ? [true, "Le format n'est pas respecté"] : [false];
-}
-
-function isRadioChecked(tabInput) {
-
-}
-
-
-  // objectif : ajouter l'attribut data-error-visible à la div formData passé en argument
+// objectif : ajouter l'attribut data-error-visible à la div formData passé en argument
 function setDataError(div, message) {
   div.setAttribute('data-error', message);
   div.setAttribute('data-error-visible', true);
 }
 
 function deleteDataError(div) {
-    div.setAttribute('data-error-visible', false);
+  div.setAttribute('data-error-visible', false);
 }
